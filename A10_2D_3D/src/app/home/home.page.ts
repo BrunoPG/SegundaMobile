c3213fe0b92a1a3a4501d5b2d4fa7d96d1e201a3
BrunoPG <brunogibicoski27@gmail.com>
2019-05-20 21:36:28 -0300

    3d2y

--- A10_2D_3D/src/app/home/home.page.ts
+++ A10_2D_3D/src/app/home/home.page.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  
 })
 export class HomePage {
+  constructor(public NavCtrl: NavController){ 
+  }
+  Start(){
 
+  }
+  Stop(){
+    
+  }
 }
