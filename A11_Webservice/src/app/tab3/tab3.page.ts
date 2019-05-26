70efb59e3096e88fd1fc6808908451d914291b38
alexandre <romeroleonardoalexandre@gmail.com>
2019-05-26 18:44:51 -0300

    ionic android

--- A11_Webservice/src/app/tab3/tab3.page.ts
+++ A11_Webservice/src/app/tab3/tab3.page.ts
@@ -1,8 +1,32 @@
 import { Component } from '@angular/core';
+import { Push, PushObject, PushOptions } from "@ionic-native/push/ngx";
+
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {}
+export class Tab3Page {
+  constructor(private push: Push){
+    this.pushsetup();
+  }
+
+  pushsetup() {
+    const options: PushOptions = {};
+
+    const pushObject: PushObject = this.push.init(options);
+
+    pushObject.on("registration").subscribe((registration: any) => {});
+
+    pushObject.on("notification").subscribe((notification: any) => {
+      if (notification.additionalData.foreground) {
+        let youralert = this.alertCtrl.create({
+          title: notification.label,
+          message: notification.message
+        });
+        youralert.present();
+      }
+    });
+  }
+}
